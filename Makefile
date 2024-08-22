95911341e5e41faf30e099026303fec1c721e594
zzjc1234 <2359047351@qq.com>
2024-08-22 21:49:23 +0800

    fix: postgres db config

---
+++ Makefile
@@ -0,0 +1,6 @@
+run:
+	python3 manage.py runserver
+clean:
+	python3 manage.py clearsession
+migrate:
+	python3 manage.py makemigrations
--- testDir/settings.py
+++ testDir/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "web",
+    "spider.apps.SpiderConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -75,11 +76,16 @@ WSGI_APPLICATION = "testDir.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'zzjc',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
