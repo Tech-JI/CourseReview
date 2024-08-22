e8a9c55e60eadbb2f9a612467311ca48ae6fa0d1
zzjc1234 <2359047351@qq.com>
2024-08-22 22:02:29 +0800

    fix: settings

--- testDir/settings.py
+++ testDir/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "web",
-    "spider.apps.SpiderConfig",
+    "spider",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
