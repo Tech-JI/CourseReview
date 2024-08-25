3e4e32e71954be76b96e00ef3df79dc5248a1a2b
Pachakutiq <null@gmail.com>
2024-08-25 14:28:27 +0800

    feat: Change email restriction to SJTU email

--- apps/web/models/student.py
+++ apps/web/models/student.py
@@ -11,18 +11,12 @@ from lib import constants
 
 class StudentManager(models.Manager):
 
-    def is_valid_dartmouth_student_email(self, email):
+    def is_valid_sjtu_student_email(self, email):
         email_components = email.split("@")
         if len(email_components) != 2:
             return False
-        username, domain = email_components
-        year = username.split(".")[-1]
-        return (
-            domain == "dartmouth.edu" and
-            len(year) == 2 and
-            (year.isdigit() or year.lower() == "ug" or year.lower() == "gr"
-                or year.lower() == "mt")
-        )
+        domain = email_components[1]
+        return (domain == "sjtu.edu.cn")
 
 
 class Student(models.Model):
@@ -35,26 +29,24 @@ class Student(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    unauth_session_ids = ArrayField(
-        base_field=models.CharField(max_length=32, unique=True), 
-        default=list,
-        blank=True
-    )
+    unauth_session_ids = ArrayField(base_field=models.CharField(max_length=32,
+                                                                unique=True),
+                                    default=list,
+                                    blank=True)
 
     def send_confirmation_link(self, request):
         full_link = request.build_absolute_uri(
             reverse('confirmation')) + '?link=' + self.confirmation_link
         if not settings.DEBUG:
-            send_mail(
-                'Your confirmation link',
-                'Please navigate to the following confirmation link: ' +
-                full_link, constants.SUPPORT_EMAIL,
-                [self.user.email], fail_silently=False
-            )
+            send_mail('Your confirmation link',
+                      'Please navigate to the following confirmation link: ' +
+                      full_link,
+                      constants.SUPPORT_EMAIL, [self.user.email],
+                      fail_silently=False)
 
     def can_see_recommendations(self):
-        return (Vote.objects.num_quality_upvotes_for_user(self.user) >=
-                constants.REC_UPVOTE_REQ)
+        return (Vote.objects.num_quality_upvotes_for_user(self.user)
+                >= constants.REC_UPVOTE_REQ)
 
     def __unicode__(self):
         return str(self.user)
