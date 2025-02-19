1f4d7e0e3c4011f9477e64a59ca50b0d9b81b231
foggystar <fdgaojx@126.com>
2025-02-19 22:07:04 +0800

    fix: Judge whether the input term is valid correctly

--- lib/terms.py
+++ lib/terms.py
@@ -9,14 +9,28 @@ def numeric_value_of_term(term):
     if term_data and term_data.group("year") and term_data.group("term"):
         year = int(term_data.group("year"))
         term = term_data.group("term")
-        return year * 10 + {"w": 1, "s": 2, "x": 3, "f": 4}[term.lower()]
+        return year * 10 + {"s": 2, "x": 3, "f": 4}[term.lower()]
     return 0
 
 
 def is_valid_term(term):
-    term_data = term_regex.match(term)
-    current_year = int(term_regex.match(constants.CURRENT_TERM).group("year"))
-    return term_data and int(term_data.group("year")) <= current_year
+    if not isinstance(term, str) or len(term) != 3:
+        return False
+    if not term[:2].isdigit():
+        return False
+    last_char = term[2].upper()
+    if last_char not in ['X', 'S', 'F']:
+        return False
+    current_term = constants.CURRENT_TERM
+    current_value = numeric_value_of_term(current_term)
+    term_value = numeric_value_of_term(term)
+    if not term_value or not current_value:
+        if term[2].upper() > current_term[2].upper():
+            return False
+        if term[2].upper() < current_term[2].upper():
+            return True
+        return True
+    return current_value >= term_value
 
 
 def split_term(term):
