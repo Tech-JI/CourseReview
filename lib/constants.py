068508842aa899cc422f65b45ec4014ba8be5105
foggystar <fdgaojx@126.com>
2025-02-19 21:58:22 +0800

    fix: Calculate the current term correctly

--- lib/constants.py
+++ lib/constants.py
@@ -1,6 +1,21 @@
 import os
+from datetime import datetime
 
-CURRENT_TERM = "16S"
+def get_current_term():
+    now = datetime.now()
+    year = str(now.year)[2:]  # Get last 2 digits of year
+    month = now.month
+    
+    if 2 <= month <= 4:
+        term = 'S'  # Spring
+    elif 5 <= month <= 8:
+        term = 'X'  # Summer
+    else:
+        term = 'F'  # Fall
+    
+    return f"{year}{term}"
+
+CURRENT_TERM = get_current_term()
 # CURRENT_TERM = os.environ["CURRENT_TERM"]  # e.g. 16S
 SUPPORT_EMAIL = "support@layuplist.com"
 REC_UPVOTE_REQ = 2
