90ff6ea427aba2368aa6973c593fc95328c1cf34
arthurcai <arthurcai@sjtu.edu.cn>
2025-09-22 18:06:52 +0800

    fix: filter box placeholder position

--- frontend/src/components/CourseList.vue
+++ frontend/src/components/CourseList.vue
@@ -57,7 +57,7 @@
                   v-model="filters.code"
                   @keyup.enter="applyFiltersAndSort"
                   placeholder="e.g., ECE215"
-                  class="mt-1 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  class="mt-1 block w-full rounded-md border-0 py-1.5 pl-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
 
