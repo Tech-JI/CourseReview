7a3cf02602107745cf8d8e9e9c1ec210836b5713
SherryShijiarui <2397377661@qq.com>
2026-01-12 22:07:42 +0800

    reset to original py

--- apps/web/tests/lib_tests/test_terms.py
+++ apps/web/tests/lib_tests/test_terms.py
@@ -7,31 +7,32 @@ from lib import constants, terms
 
 class TermsTestCase(TestCase):
     def test_term_regex_works_in_common_case(self):
-        term_data = terms.term_regex.match("16F")
+        term_data = terms.term_regex.match("16W")
         self.assertTrue(
             term_data
             and term_data.group("year") == "16"
-            and term_data.group("term") == "F"
+            and term_data.group("term") == "W"
         )
 
     def test_term_regex_only_allows_two_digit_years(self):
-        term_data = terms.term_regex.match("2016F")
+        term_data = terms.term_regex.match("2016W")
         self.assertFalse(term_data)
 
     def test_term_regex_disallows_bad_terms(self):
         self.assertFalse(terms.term_regex.match("16a"))
 
     def test_term_regex_allows_for_lower_and_upper_terms(self):
-        term_data = terms.term_regex.match("16F")
+        term_data = terms.term_regex.match("16W")
         self.assertTrue(
             term_data
             and term_data.group("year") == "16"
-            and term_data.group("term") == "F"
+            and term_data.group("term") == "W"
         )
-        term_data = terms.term_regex.match("16F")
+        term_data = terms.term_regex.match("16w")
         self.assertTrue(
             term_data
             and term_data.group("year") == "16"
+            and term_data.group("term") == "w"
             and term_data.group("term") == "F"
         )
 
@@ -48,11 +49,11 @@ class TermsTestCase(TestCase):
         self.assertEqual(terms.numeric_value_of_term("asd"), 0)
         self.assertEqual(terms.numeric_value_of_term("2001"), 0)
         self.assertEqual(terms.numeric_value_of_term("1s"), 0)
-        self.assertEqual(terms.numeric_value_of_term("2016F"), 0)
+        self.assertEqual(terms.numeric_value_of_term("2016w"), 0)
         self.assertEqual(terms.numeric_value_of_term("fall"), 0)
 
     def test_numeric_value_of_term_ranks_terms_in_correct_order(self):
-        correct_order = ["", "09F", "09S", "09X", "12F", "14x", "15F", "16S", "20x"]
+        correct_order = ["", "09w", "09S", "09X", "12F", "14x", "15w", "16S", "20x"]
         shuffled_data = list(correct_order)
         while correct_order == shuffled_data:
             random.shuffle(shuffled_data)
@@ -63,7 +64,7 @@ class TermsTestCase(TestCase):
         self.assertEqual(correct_order, sorted_data)
 
     def test_numeric_value_of_term_gives_expected_numeric_value(self):
-        self.assertEqual(terms.numeric_value_of_term("16F"), 161)
+        self.assertEqual(terms.numeric_value_of_term("16W"), 161)
 
     def test_is_valid_term_returns_false_if_in_future(self):
         next_year = (
@@ -75,7 +76,7 @@ class TermsTestCase(TestCase):
         self.assertFalse(terms.is_valid_term(""))
 
     def test_is_valid_term_returns_false_if_no_year(self):
-        self.assertFalse(terms.is_valid_term("F"))
+        self.assertFalse(terms.is_valid_term("w"))
 
     def test_is_valid_term_returns_true_for_current_term(self):
         self.assertTrue(terms.is_valid_term(constants.CURRENT_TERM))
